68d993e566634c6b53c77d96528d56885bb03d28
Pierretoson <pierre.toson@glovoapp.com>
2020-07-02 17:45:58 +0200

    no

--- js/script.js
+++ js/script.js
@@ -126,7 +126,7 @@ function buildAndShowHomeHTML (categories) {
       // of how to do that.
       // ....
 
-      insertHtml("#specials", homeHtmlToInsertIntoMainPage);
+      insertHtml("#main-content", homeHtmlToInsertIntoMainPage);
 
 
     },
